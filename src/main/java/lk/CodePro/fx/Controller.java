61b8fbdb07cc887939b884ba3c5edcacbf5e0719
Sam1ndaFernando <uthpala2001@gmail.com>
2024-07-30 22:33:54 +0530

    implementing the user interface

---
+++ src/main/java/lk/CodePro/fx/Controller.java
@@ -0,0 +1,29 @@
+package lk.CodePro.fx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class Controller {
+
+    @FXML
+    private Button getIpBtn;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Button ipTrackerBtn;
+
+    @FXML
+    void getterButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void trackerButton(ActionEvent event) {
+
+    }
+
+}
