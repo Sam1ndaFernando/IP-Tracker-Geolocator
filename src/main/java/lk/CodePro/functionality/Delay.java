5cc03657f70ee9ecd9c0e15438c04e2fc867b897
Sam1ndaFernando <uthpala2001@gmail.com>
2024-07-31 21:31:10 +0530

    Add functionality for delaying image visibility in Delay class.

---
+++ src/main/java/lk/CodePro/functionality/Delay.java
@@ -0,0 +1,23 @@
+package lk.CodePro.functionality;
+
+import javafx.scene.image.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Delay {
+
+    public void copiedDelay(int delay, ImageView img) {
+
+        Timer timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                img.setVisible(false);
+            }
+        }, delay);
+
+    }
+
+}
